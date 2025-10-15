cd4c4c985b991b69eff525b63236c7d0bf3d99e4
Faizan <faizanvohra382@gmail.com>
2025-10-15 18:57:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Todo from "./Componetns/toDo";
+import Clock from "./Componetns/Clock/Clock";
+import List from "./Componetns/list";
+import Map from "./Componetns/map";
+function App() {
+  let foodItem = ["hero", " ha", "dss"];
+  return (
+    <>
+      <h1>Hello Brother</h1>
+
+      
+      
+      <Todo/>
+   
+      </>
+  );
+}
+
+export default App;
